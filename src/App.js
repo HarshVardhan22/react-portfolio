b9eaa132d100c5309f66331de8ccb2b012aedf55
Harshvardhan <67060748+HarshVardhan22@users.noreply.github.com>
2021-08-22 23:25:19 +0530

    +created folder structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Home from "./Components/Home/Home"
+import Projects from "./Components/Projects/Projects"
+import Blogs from "./Components/Blogs/Blogs"
+import { BrowserRouter,Switch,Route } from 'react-router-dom'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      <Switch>
+        
+      </Switch>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
