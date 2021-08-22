73827374b3f6f4b858e68044720e8f60865ffde9
Harshvardhan <67060748+HarshVardhan22@users.noreply.github.com>
2021-08-22 23:27:51 +0530

    +implemented routing

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
-import React from 'react'
-import Home from "./Components/Home/Home"
-import Projects from "./Components/Projects/Projects"
-import Blogs from "./Components/Blogs/Blogs"
-import { BrowserRouter,Switch,Route } from 'react-router-dom'
+import React from "react";
+import Home from "./Components/Home/Home";
+import Projects from "./Components/Projects/Projects";
+import Blogs from "./Components/Blogs/Blogs";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 const App = () => {
   return (
     <BrowserRouter>
+      <Navbar />
       <Switch>
-        
+        <Route path="/" exact><Home/></Route>
+        <Route path="/projects"><Projects/></Route>
+        <Route path ="/blogs"><Blogs/></Route>
       </Switch>
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default App
+export default App;
